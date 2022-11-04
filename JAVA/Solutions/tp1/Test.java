5b5edc5d9a162501e628fcf08ba1cf1b4dbb7c6e
NGUENA ZEBS <94785948+PILOTEZEBS@users.noreply.github.com>
2022-11-04 21:53:14 +0100

    Add files via upload

---
+++ JAVA/Solutions/tp1/Test.java
@@ -0,0 +1,12 @@
+package tp1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int[] a=new int[10];
+		int b=a.length;
+		Tri tab1=new Tri(a, b);
+		
+	}
+
+}
---
+++ JAVA/Solutions/tp1/Tri.java
@@ -0,0 +1,24 @@
+package tp1;
+
+public class Tri {
+
+	private int[] tab;
+	private int taille=tab.length;
+	
+	public Tri() {
+		
+	}
+	
+	public Tri(int[] t, int n){
+		this.tab=t;
+		this.taille=n;
+	}
+	
+	public void triSelection() {
+		//CODE DU TRI PAR SELECTION VOIR ALGO
+	}
+	public void triInsertion() {
+		//CODE DU TRI PAR INSERTION VOIR ALGO
+	}
+	
+}
